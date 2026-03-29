272809fb11be2bf24c3862be609f471343db16a4
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-03-29 16:02:22 +0200

    added: flake import for home-manager 25.11

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
     import-tree.url = "github:vic/import-tree";
 
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
