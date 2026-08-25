2084778fb11fca7c35cf9819eaa4300fb681955c
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-08-25 04:16:44 +0200

    disabled: autofirma until upstream updates to 26.05 solution

--- modules/features/autofirma.nix
+++ modules/features/autofirma.nix
@@ -3,8 +3,18 @@
     imports = [ inputs.autofirma-nix.nixosModules.default ];
     
     programs.autofirma = {
-        enable = true;
-        firefoxIntegration.enable = true;
+      enable = false;
+      firefoxIntegration.enable = false;
+      package = let
+        pom-tools = pkgs.callPackage "${inputs.autofirma-nix}/nix/tools/pom-tools" {};
+        jmulticard-fixed = pkgs.callPackage "${inputs.autofirma-nix}/nix/autofirma/dependencies/jmulticard" {
+          inherit pom-tools;
+          src = inputs.autofirma-nix.inputs.jmulticard-src;
+          maven-dependencies-hash = "sha256-2lUqrN8s0KTbk8wd76FkU5wgaPZnzmpO9rgTE6Oe+os=";
+        };
+      in inputs.autofirma-nix.packages.${pkgs.system}.autofirma.override {
+        jmulticard = jmulticard-fixed;
+      };
     };
 
     # DNIeRemote integration for using phone as NFC reader
@@ -25,7 +35,7 @@
         enable = true;
         policies.SecurityDevices = {
         "OpenSC PKCS#11" = "${pkgs.opensc}/lib/opensc-pkcs11.so";
-        "DNIeRemote" = "${config.programs.dnieremote.finalPackage}/lib/libdnieremotepkcs11.so";
+        #"DNIeRemote" = "${config.programs.dnieremote.finalPackage}/lib/libdnieremotepkcs11.so";
         };
     };
 
